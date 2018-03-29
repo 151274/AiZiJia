d2be1f0ff808f8824e079b3b95aafdaa5b64cb3c
習慣沉默 <2689921152@qq.com>
2018-03-29 20:58:11 +0800

    update DB.class.php

--- Public/DB/DB.class.php
+++ Public/DB/DB.class.php
@@ -14,7 +14,7 @@ class DB
 
         $dsn=sprintf("%s:host=%s;dbname=%s","mysql","mysql.coding.io","db-UXTKLDlW3H");
       	
-        self::$pdo=new PDO($dsn,"user-wE5V3ZfCPD","QWydO3s8kQ9sbEBc");
+        self::$pdo=new PDO($dsn,"user-wE5V3ZfCPD","Nj5M[(HNm@:k/!K}`j$T");
         self::$pdo->query("set names utf8");
 
 
